b4a9124471adbef98539915bbefc5b8d69feeab0
Vincent McNabb <mcnabb.vincent@gmail.com>
2018-01-14 17:39:00 +1300

    Add feature - shift+backspace will remove the last jamo from the character left of the caret.

--- selectionEditor.js
+++ selectionEditor.js
@@ -7,9 +7,9 @@
             if(element.selectionStart != undefined) {
                 const start = element.selectionStart;
                 let end = element.selectionEnd;
-                element.value = element.value.substring(0, start) +
-                    text +
-                    element.value.substring(end, element.value.length);
+                element.value = element.value.substring(0, start)
+                    + text
+                    + element.value.substring(end, element.value.length);
                 end = start + text.length;
                 element.selectionStart = start;
                 element.selectionEnd = end;
@@ -26,11 +26,11 @@
                 selected = { range, selection };
             }
         }
-    
+
         const deselect = this.deselect = function() {
             if (element.selectionStart != undefined) {
                 element.selectionStart = element.selectionEnd;
-    
+
             } else if (selected) {
                 selected.range.collapse(false);
                 selected.selection.removeAllRanges();
@@ -38,7 +38,7 @@
                 selected = undefined;
             }
         }
-    
+
         this.restore = () => {
             if (selected) {
                 // [contenteditable]
@@ -48,10 +48,36 @@
                 selection.addRange(selected.range);
             }
         };
-    
+
         this.insert = function(text) {
             replace(text);
             deselect();
         };
+
+        this.selectPreviousCharacter = function () {
+            if (element.selectionStart !== undefined) {
+                // input[type=text]
+                const start = element.selectionStart - 1;
+                const end = start + 1;
+
+                if (start >= 0) {
+                    element.selectionStart = start;
+                    element.selectionEnd = end;
+                    selection = { start, end }
+                    return element.value.substr(start, 1);
+                }
+
+            } else {
+                // [contentEditable]
+                const selection = element.ownerDocument.getSelection();
+                const startOffset = selection.focusOffset;
+
+                if (selection.type === "Caret" && startOffset > 0) {
+                    selection.getRangeAt(0).setStart(selection.focusNode, startOffset - 1);
+                    return selection.focusNode.nodeValue.substr(startOffset - 1, 1);
+
+                } else return false;
+            }
+        }
     }
 })(window.koreanIme);
