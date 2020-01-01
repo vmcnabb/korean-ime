032d63ecdbe45e4e7d9327215536ea02c809632e
Vincent McNabb <mcnabb.vincent@gmail.com>
2020-01-02 08:57:52 +1300

    create and use GoogleDocsSelectionEditor

--- src/selectionEditorFactory.js
+++ src/selectionEditorFactory.js
@@ -1,5 +1,6 @@
 import { InputSelectionEditor } from "./inputSelectionEditor.js";
 import { ContentEditableSelectionEditor } from "./contentEditableSelectionEditor.js"
+import { GoogleDocsSelectionEditor } from "./googleDocsSelectionEditor.js";
 
 export class SelectionEditorFactory {
     static createSelectionEditor (element) {
@@ -7,6 +8,10 @@ export class SelectionEditorFactory {
             return new InputSelectionEditor(element);
 
         } else if (element.isContentEditable) {
+            if (window.frameElement && window.frameElement.classList.contains("docs-texteventtarget-iframe")) {
+                return new GoogleDocsSelectionEditor(element);
+            }
+
             return new ContentEditableSelectionEditor(element);
         }
     }
