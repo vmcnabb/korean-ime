278a36e01ddb376effdc001daef7f002a2b4bd34
Vincent McNabb <mcnabb.vincent@gmail.com>
2020-01-12 13:59:19 +1300

    complete keyboard layout

--- src/popupKeyboard/index.js
+++ src/popupKeyboard/index.js
@@ -3,9 +3,10 @@
 import m from "../mappings/koreanKeyboardMap";
 
 const layout = [
-    [m.KeyQ, m.KeyW, m.KeyE, m.KeyR, m.KeyT, m.KeyY, m.KeyU, m.KeyI, m.KeyO, m.KeyP, m.BracketLeft, m.BracketRight],
-    [m.KeyA, m.KeyS, m.KeyD, m.KeyF, m.KeyG, m.KeyH, m.KeyJ, m.KeyK, m.KeyL],
-    [m.KeyZ, m.KeyX, m.KeyC, m.KeyV, m.KeyB, m.KeyN, m.KeyM]
+    ["Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Minus", "Equals", "Backspace"],
+    ["Tab", "KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Backslash"],
+    ["CapsLock", "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "Enter"],
+    ["Shift", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period", "Slash", "ShiftRight"]
 ];
 
 setupKeyboard();
@@ -16,9 +17,39 @@ function setupKeyboard() {
         const rowElement = document.createElement("div");
         rowElement.className = "row";
 
-        row.forEach(key => {
+        row.forEach(keyName => {
             const keyElement = document.createElement("kbd");
-            keyElement.innerHTML = key;
+            const key = m[keyName];
+
+            keyElement.className = keyName;
+
+            const shiftLabel = document.createElement("div");
+            shiftLabel.className = "shift";
+            shiftLabel.innerText = key.shift || key.label;
+            keyElement.appendChild(shiftLabel);
+
+            if (!key.normal) {
+
+            } else if (!key.jamo) {
+                const baseLabel = document.createElement("div");
+                baseLabel.className = "base";
+                baseLabel.innerText = key.normal;
+                keyElement.appendChild(baseLabel);
+
+            } else {
+                const baseJamo = document.createElement("div");
+                baseJamo.className = "base jamo";
+                baseJamo.innerText = key.jamo.normal;
+                keyElement.appendChild(baseJamo);
+
+                if (key.jamo.shift) {
+                    const shiftJamo = document.createElement("div");
+                    shiftJamo.className = "shift jamo";
+                    shiftJamo.innerText = key.jamo.shift;
+                    keyElement.appendChild(shiftJamo);
+                }
+            }
+
             rowElement.appendChild(keyElement);
         });
 
