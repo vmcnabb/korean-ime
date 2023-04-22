3292c9432162439e179d891c921f0e33a678b64a
Vincent McNabb <mcnabb.vincent@gmail.com>
2023-04-23 02:45:38 +1200

    Allow event watcher to be quiet

--- research/watch-events.js
+++ research/watch-events.js
@@ -1,7 +1,7 @@
 // call afterLoad when document is idle
-document.addEventListener("DOMContentLoaded", afterLoad);
+document.addEventListener("DOMContentLoaded", onLoad);
 
-function afterLoad() {
+function onLoad() {
     const inputDiv = document.getElementById("input");
 
     // array of all input events
@@ -20,11 +20,19 @@ function afterLoad() {
         "selectstart"
     ];
 
+    let silence = true;
     let lastInnerText = "";
+
+    // add event listeners to silence checkbox
+    document.getElementById("silence").addEventListener("change", e => {
+        silence = e.target.checked;
+    });
 
     // add event listeners to input div
     inputEvents.forEach(eventKey => {
         document.addEventListener(eventKey, e => {
+            if (silence) return;
+
             let innerText = inputDiv.innerText;
             if (innerText !== lastInnerText) {
                 lastInnerText = innerText;
