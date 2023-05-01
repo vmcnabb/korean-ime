b7db1edb40810b8a1c3234d31e75659d4019cd24
Vincent McNabb <mcnabb.vincent@gmail.com>
2023-05-01 19:30:15 +1200

    Ensure that SupportedCompositionFeatures returns correct method names

---
+++ src/composition/composition-adapters/dummy-adapter.ts
@@ -0,0 +1,33 @@
+// import { CompositionAdapter } from "./composition-adapter";
+
+/**
+ * This adapter is used by Composition adapter to get a list of all methods that exist in CompositionAdapter.
+ * We need this because CompositionAdapter is an abstract class, and TypeScript doesn't allow us to get a list of
+ * methods from an abstract class.
+  */
+export class DummyAdapter { // extends CompositionAdapter {
+    blur(): void {
+        throw new Error("Method not implemented.");
+    }
+    collapseSelection(): void {
+        throw new Error("Method not implemented.");
+    }
+    getPreviousCharacter(): string | undefined {
+        throw new Error("Method not implemented.");
+    }
+    deleteContentBackwards(): void {
+        throw new Error("Method not implemented.");
+    }
+    inputCharacter(): void {
+        throw new Error("Method not implemented.");
+    }
+    beginComposition(): void {
+        throw new Error("Method not implemented.");
+    }
+    updateComposition(): void {
+        throw new Error("Method not implemented.");
+    }
+    endComposition(): void {
+        throw new Error("Method not implemented.");
+    }
+}
