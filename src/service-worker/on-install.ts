4357d015f134a8177716b89e42d1643c7b5eaaa5
Vincent McNabb <mcnabb.vincent@gmail.com>
2023-05-01 21:42:16 +1200

    Only update state display from StateManager.

--- src/service-worker/on-install.ts
+++ src/service-worker/on-install.ts
@@ -2,6 +2,5 @@ import { createMenus } from "./menus";
 
 export function onInstall(_details: chrome.runtime.InstalledDetails): void {
     console.debug("onInstall _details: ", _details);
-
     createMenus();
 }
