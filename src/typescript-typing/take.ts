b2e1a4fa3d723b6c1737a2e612950631dcce9be7
Vincent McNabb <mcnabb.vincent@gmail.com>
2023-05-01 19:26:40 +1200

    Switch build system to Parcel

--- src/typescript-typing/take.ts
+++
@@ -1,7 +0,0 @@
-export function Take<T, K extends keyof T>(obj: T, ...keys: K[]): Pick<T, K> {
-    const result = {} as Pick<T, K>;
-    keys.forEach(key => {
-        result[key] = obj[key];
-    });
-    return result;
-}
